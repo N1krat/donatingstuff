948d4d3c4dda7b128dae4d9f6d18ad438f9dd602
N1krat <bogdanbraniste2007@gmail.com>
2025-01-21 00:15:35 +0200

    added a new card and changed the navbar style

--- src/components/views/mainPage.vue
+++ src/components/views/mainPage.vue
@@ -1,39 +1,43 @@
 <template>
   <!-- NavBar -->
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img :src="require('@/assets/loli.png')" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-        <b>DonatingStuff</b>
-      </a>
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/registration"><b>Registration</b></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/dashboard"><b>Dashboard</b></router-link>
-          </li>
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            EN
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">RU</a></li><br>
-            <li><a class="dropdown-item" href="#">RO</a></li><br>
-            <li><a class="dropdown-item" href="#">DE</a></li>
-          </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+  <div class="container-fluid" id="navbar">
+    <router-link class="navbar-brand" to="/">
+      <img :src="require('@/assets/loli.png')" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Navbar
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/registration">Registration</router-link>
         </li>
-        </ul>
-      </div>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
 
+
+  <!-- main page -->
   <div class="container-fluid" id="con"> 
-    <h1>Velcome to DonatingStuff!</h1>
-    <h3>Made by me</h3>
-    <img :src="require('@/assets/loli.png')" /> 
+    <div class="cards" id="card1">
+      <div class="card-content">
+        <div class="text">
+        <h3 class="card-title">Card title</h3>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <router-link to="/registration" class="btn btn-primary">Get Started</router-link>
+          <router-link href="#" class="btn btn-secondary">Go somewhere</router-link>
+        </div>
+      </div>
+       <div class="card-image">
+             <img src="https://placehold.co/400x300" class="card-img-top" alt="...">
+        </div>
+    </div>
   </div>
 </template>
 
@@ -41,6 +45,7 @@
 export default {
   name: 'mainPage',
 }
+
 </script>
 
 <style scoped>
@@ -68,14 +73,21 @@ a {
   margin-top: 60px;
 }
 
-#con { 
-  background-color: #9bb186;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
 /* navbar css */
+#navbar { 
+  background-color: #56638A;
+  margin: 0px;
+  padding: 0px;
+  color: white;
+  padding-bottom: 10px;
+  padding-top: 5px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 5px;
+  
+}
+
 #navbarNav {
   justify-content: flex-end;
 }
@@ -83,11 +95,40 @@ a {
 a { 
   text-decoration: none; 
   padding: 5px;
+  color: white; 
+  padding: 10px; 
+  
 }
 
 a:hover { 
   box-shadow: #2c3e50;
+  color: black;
   background-color: rgb(190, 190, 190); 
 }
+
+/* card css */
+#card1 { 
+  background-color: #56638A;
+  margin: 20px;
+  padding: 20px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+  border: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.6);
+  gap: 40px;
+  
+
+  
+} 
+
+
 </style>
 
