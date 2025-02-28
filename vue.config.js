0eb5b7be3760cda735aef8e93711fabe7323b497
N1krat <bogdanbraniste2007@gmail.com>
2025-02-28 08:51:32 +0200

    sgs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true, 
-  publicPath: process.env.NODE_ENV === "production" ? "/donatingstuff/" : "/",
+  base: "/REPONAME/",
+  plugins: [],
+  resolve: {
+    alias: [
+      
+    ]
+  },
 })
