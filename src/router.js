5e0dad1c51837ded74ad0416aaeda8fab1863980
N1krat <bogdanbraniste2007@gmail.com>
2025-02-28 09:29:18 +0200

    gf

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import mainPage from './components/views/mainPage.vue'
 import registrationPage from './components/views/registrationPage.vue'
@@ -82,7 +82,6 @@ const routes = [
 
 const router = createRouter({
     history: createWebHashHistory(),
-    mode: 'hash',
     routes,
 })
 
