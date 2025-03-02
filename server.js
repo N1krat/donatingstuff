7b3cd793f14e2f18d1c2ff3919661d852723353b
N1krat <bogdanbraniste2007@gmail.com>
2025-03-02 23:38:33 +0200

    sfs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const path = require('path');
 
 const app = express();
 app.use(cors());
