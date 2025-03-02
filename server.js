9366f4b72f5b4481caaa460478d876e2c17f4767
N1krat <bogdanbraniste2007@gmail.com>
2025-03-02 23:21:59 +0200

    server change

--- server.js
+++ server.js
@@ -250,4 +250,5 @@ app.get('/dashboardDonations', (req, res) => {
 
 
 // Start the server
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server running on http://localhost:3000'));
