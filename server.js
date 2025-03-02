08b5a5333cadeb2c352f3b7516fb3a1a577b6e7d
N1krat <bogdanbraniste2007@gmail.com>
2025-03-03 00:35:05 +0200

    hh

--- server.js
+++ server.js
@@ -241,5 +241,5 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
